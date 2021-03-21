3736cb1a66f7a20d4440b47ea9e446b41b14aece
JavierVeg <76919238+JavierVeg@users.noreply.github.com>
2021-03-21 17:16:38 +0100

    calculo de errores

--- Trabajo 2. Modelos explicitos/Scripts/Panel_Current.m
+++ Trabajo 2. Modelos explicitos/Scripts/Panel_Current.m
@@ -3,15 +3,20 @@ global V_oc
 global I_sc
 global V_mp
 global I_mp
+global model
 
+switch(model)
+case 1 
 % modelo Das
-% I_modelo =  I_sc*(( 1 - ( V / V_oc )^u(1) ) / ( 1 + u(2) * ( V / V_oc )) );
-    
+I_modelo =  I_sc*(( 1 - ( V / V_oc )^u(1) ) / ( 1 + u(2) * ( V / V_oc )) );
+
+case 2
 %  Karmalkar & Hannefa's model
 gamma = u(1);
 m = u(2);
 I_modelo =  I_sc*(1 - (1- gamma)* (V/V_oc) - gamma * (V/V_oc)^m);
 
+case 3
 % Pindado & Cubas's model
 eta = u(1);
 
@@ -21,5 +26,6 @@ end
 
 if V>=V_mp
 I_modelo = I_mp*(V_mp/V)*(1 - ((V - V_mp)/(V_oc - V_mp))^eta); 
+end
 end
 
--- Trabajo 2. Modelos explicitos/Scripts/RECT.m
+++ Trabajo 2. Modelos explicitos/Scripts/RECT.m
@@ -3,5 +3,5 @@ global n
 for i=1:size(V,2)
 	I_modelo(i) = Panel_Current(u,V(i));
 end
-error = ((sum((I_modelo - I_exp).^2)/n)^0.5)%/sum(I_exp);
+error = ((sum((I_modelo - I_exp).^2)/n)^0.5); %/sum(I_exp);
 
