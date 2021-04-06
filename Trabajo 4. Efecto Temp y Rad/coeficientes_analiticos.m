31feb2b4f7d3a1dffb7b54a4288029c95e4ece2c
Diego <diego.mataix@hotmail.com>
2021-04-06 23:44:11 +0200

    He creado unas funciones para los modelos, por si los volvemos a usar
    - He creado unas funciones para los modelos, por si los volvemos a usar
    - He puesto la temperatura como vector

--- Trabajo 4. Efecto Temp y Rad/coeficientes_analiticos.m
+++ Trabajo 4. Efecto Temp y Rad/coeficientes_analiticos.m
@@ -1,8 +1,8 @@
 clear all; clc; close all
 
 %% DATOS
-%Orden de los datos (FichaTecnica:FT) -> FT1BOL, FT1_2.5E14, FT1_2.5E14, FT1_2.5E14, Experimental
-%                                        [ Isc, Imp, Vmp, Voc, betha, alpha, k]
+% Orden de los datos (FichaTecnica:FT) -> FT1BOL, FT1_2.5E14, FT1_2.5E14, FT1_2.5E14, Experimental
+% [ Isc, Imp, Vmp, Voc, betha, alpha]
 
 load('z.mat')
 dat_exp = z;
@@ -23,34 +23,82 @@ for i = 1: size(dat,2)
     V(i,:) = linspace(0,dat(4,i),200);
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+caso = 1;              %   1: Modelo explícito K&H         2: Modelo 1D2R
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% Resultados experimentales
-
-caso = 1;
 switch(caso)
-    
+    %% Modelo de Kalmalkar-Haneefa
     case 1
+        %Ajuste mediante dos parametros gamma_k y m
+        % i = 1 - (1- gamma)* v - gamma * v^m
+        % i y v son la intensidad y el voltaje en forma adimensional.
+        %%% Determine coefficients %%%
+        [C, m_func, m, gamma, I_Kar] = KH_model(dat,V, dat(1,:), dat(4,:), dat(5,:), dat(6,:));
+        
+        %%% PLOT %%%
+        myplot(I_Kar, V, dat, dat_exp)
+        
+        %% Modelo 1D2R
+    case 2
+        [Vt, I] = UND2R(dat, V, Isc,Voc,Imp,Vmp,n);
+        %%% PLOT %%%
+        myplot(I, V, dat, dat_exp)
+end
 
-%% Modelo de Kalmalkar-Haneefa
+%% Funciones
+function [Ipv,I0,Rs,Rsh] = param_1D_2R_Lap(Isc,Voc,Imp,Vmp,a,Vt)
 
-%Ajuste mediante dos parametros gamma_k y m
-% i = 1 - (1- gamma)* v - gamma * v^m
-% i y v son la intensidad y el voltaje en forma adimensional.
-gamma = zeros(1,size(dat,2));           % matriz de 1x(n de datos) para el coeficiente k del modelo de Das
-m = zeros(1,size(dat,2));
-m_simp = zeros(1,size(dat,2));
-m_func = zeros(1,size(dat,2));
-C = zeros(1,size(dat,2));
+A=-(2*Vmp-Voc)/(a*Vt)+(Vmp*Isc-Voc*Imp)/(Vmp*Isc+Voc*(Imp-Isc));
+B=-Vmp*(2*Imp-Isc)/(Vmp*Isc+Voc*(Imp-Isc));
+C=a*Vt/Imp;
+D=(Vmp-Voc)/(a*Vt);
 
+M1=0.3361;
+M2=-0.0042;
+M3=-0.0201;
+sigma = -1-log(-B)-A;
+Wn =-1-sigma -2/M1* (1-1./(1+M1*sqrt(sigma/2)./(1+M2*sigma.*exp(M3*sqrt(sigma)))) );
+
+Rs=C*(Wn-(D+A));
+Rsh=(Vmp-Imp*Rs)*(Vmp-Rs*(Isc-Imp)-a*Vt)/((Vmp-Imp*Rs)*(Isc-Imp)-a*Vt*Imp);
+Ipv=(Rsh+Rs)/Rsh*Isc;
+I0=((Rsh+Rs)/Rsh*Isc-Voc/Rsh)/(exp((Voc)/(a*Vt)));
+
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [Vt, I] = UND2R(dat, V, Isc,Voc,Imp,Vmp,n)
+k = 1.3806503e-23;   % Boltzmann [J/K]
+q = 1.60217646e-19;  % Carga electron [C]
+
+T  = [20, 20, 20, 20, 28] + 273.15;     % Temperatura nominal [K]
+Vt = n.*k.*T./q;                        % Voltaje termico
+a_n = 1.3;
+for i = 1:size(dat,2)
+    a(i) = a_n;
+    [Ipv(i),I0(i),Rs(i),Rsh(i)] = param_1D_2R_Lap(Isc(i),Voc(i),Imp(i),Vmp(i),a(i),Vt(i));
+end
+
+% Calculo I-V mediante funcion de Lambert W
+for i = 1:size(dat,2)     % bucle para los distintos casos
+    for j = 1:size(V,2)   % bucle para el voltaje
+        I(i,j) = (Rsh(i)*(Ipv(i) + I0(i)) - V(i,j))/(Rs(i) + Rsh(i)) - ((a(i)*Vt(i))/Rs(i))*...
+            lambertw(0,(((Rs(i)*Rsh(i)*I0(i))/((a(i)*Vt(i))*(Rs(i) + Rsh(i))))*exp((Rsh(i)*...
+            (Rs(i)*Ipv(i) + Rs(i)*I0(i) + V(i,j)))/(a(i)*Vt(i)*(Rs(i) + Rsh(i))))));
+    end
+end
+
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [C, m_func, m, gamma, I_Kar] = KH_model(dat,V, Isc, Voc, betha, alpha)
+%[ Isc, Imp, Vmp, Voc, betha, alpha, k]
 %%% Determine coefficients %%%
 for i = 1:size(dat,2)
     
-    C(i) = (1- dat(5,i) - dat(6,i))/ ((2*dat(5,i))-1);
-    
-    m_func(i) = lambertw(-1, ((dat(6,i)* (C(i)^-1) * log(dat(6,i)))/C(i)));   %
-    m(i)= (m_func(i)/log(dat(6,i))) + (C(i)^-1) + 1 ; % iterar para cada uno de los paneles
-    
-    gamma(i) = (2*dat(5,i) -1)/ ((dat(6,i)^m(i) *(m(i)-1)));
+    C(i) = (1- betha(1,i) - alpha(1,i))/ ((2*betha(1,i))-1);
+    m_func(i) = lambertw(-1, ((alpha(1,i)* (C(i)^-1) * log(alpha(1,i)))/C(i)));   %
+    m(i)= (m_func(i)/log(alpha(1,i))) + (C(i)^-1) + 1 ; % iterar para cada uno de los paneles
+    gamma(i) = (2*betha(1,i) -1)/ ((alpha(1,i)^m(i) *(m(i)-1)));
     
 end
 
@@ -58,20 +106,19 @@ end
 I_Kar = zeros(size(dat,2),size(V,2));
 for i = 1:size(dat,2)
     for j = 1: size(V,2)
-        I_Kar(i,j) = dat(1,i) * ( 1 - (1- gamma(i))*(V(i,j)/dat(4,i)) - gamma(i)* ( (V(i,j)/dat(4,i))^m(i) ));
+        I_Kar(i,j) = Isc(1,i) * ( 1 - (1- gamma(i))*(V(i,j)/Voc(1,i)) - gamma(i)* ( (V(i,j)/Voc(1,i))^m(i) ));
     end
 end
-
-%%% PLOT %%%
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function myplot(I, V, dat, dat_exp)
 for i = 1: size(dat,2)
-    
     % Plot I-V
     figure()
     hold on
     grid on
     box on
-    % axis([V(i,1) V(i,end)  I_das(i,1)+1  0])
-    plot(  V(i,:) , I_Kar(i,:),'-k','LineWidth',2)
+    plot(  V(i,:) , I(i,:),'-k','LineWidth',2)
     plot(dat_exp(:,1), dat_exp(:,2), '--k')
     axis tight
     axis([0 dat(4,i)*1.2 0 dat(1,i)*1.2])
@@ -82,77 +129,4 @@ for i = 1: size(dat,2)
     set(gca,'FontSize',18)
     hold off
 end
-
-%% Modelo 1D2R
-
-    case 2
-        
-% Constantes
-
-k = 1.3806503e-23;   %Boltzmann [J/K]
-q = 1.60217646e-19;  %Carga electron [C]
-
-T  = 20 + 273.15;    %Temperatura nominal [K]
-Vt = n*k*T/q;        %Voltaje termico  
-
-a_n = 1.3; 
-
-for i = 1:size(dat,2)
-    a(i) = a_n;
-    [Ipv(i),I0(i),Rs(i),Rsh(i)] = param_1D_2R_Lap(Isc(i),Voc(i),Imp(i),Vmp(i),a(i),Vt);   
 end
-
-% Calculo I-V mediante funcion de Lambert W
-
-for i = 1:size(dat,2)     % bucle para los distintos casos
-    for j = 1:size(V,2)   % bucle para el voltaje      
-    I(i,j) = (Rsh(i)*(Ipv(i) + I0(i)) - V(i,j))/(Rs(i) + Rsh(i)) - ((a(i)*Vt)/Rs(i))*...
-        lambertw(0,(((Rs(i)*Rsh(i)*I0(i))/((a(i)*Vt)*(Rs(i) + Rsh(i))))*exp((Rsh(i)*...
-        (Rs(i)*Ipv(i) + Rs(i)*I0(i) + V(i,j)))/(a(i)*Vt*(Rs(i) + Rsh(i))))));  
-    end
-end
-
-%%% PLOT %%%
-
-for i = 1: size(dat,2)  
-    % Plot I-V
-    figure()
-    hold on
-    grid on
-    box on
-    % axis([V(i,1) V(i,end)  I_das(i,1)+1  0])
-    plot(V(i,:) , I(i,:),'-k','LineWidth',2)
-    plot(dat_exp(:,1), dat_exp(:,2), '--k')
-    axis tight
-    axis([0 dat(4,i)*1.2 0 dat(1,i)*1.2])
-    xlabel('{\it V} [V]')
-    ylabel('{\it I} [A]');
-    legend({'Modelo completo','Resultados experimentales'},'Location','northeast','NumColumns',2)
-    box on
-    set(gca,'FontSize',18)
-    hold off
-end
-
-end
-
-%% Funciones
-
- function [Ipv,I0,Rs,Rsh] = param_1D_2R_Lap(Isc,Voc,Imp,Vmp,a,Vt)
-
-    A=-(2*Vmp-Voc)/(a*Vt)+(Vmp*Isc-Voc*Imp)/(Vmp*Isc+Voc*(Imp-Isc));
-    B=-Vmp*(2*Imp-Isc)/(Vmp*Isc+Voc*(Imp-Isc));
-    C=a*Vt/Imp;
-    D=(Vmp-Voc)/(a*Vt);
-    
-    M1=0.3361;
-    M2=-0.0042;
-    M3=-0.0201;
-    sigma = -1-log(-B)-A;
-    Wn =-1-sigma -2/M1* (1-1./(1+M1*sqrt(sigma/2)./(1+M2*sigma.*exp(M3*sqrt(sigma)))) );
-    
-    Rs=C*(Wn-(D+A));
-    Rsh=(Vmp-Imp*Rs)*(Vmp-Rs*(Isc-Imp)-a*Vt)/((Vmp-Imp*Rs)*(Isc-Imp)-a*Vt*Imp);
-    Ipv=(Rsh+Rs)/Rsh*Isc;
-    I0=((Rsh+Rs)/Rsh*Isc-Voc/Rsh)/(exp((Voc)/(a*Vt)));
-    
- end
