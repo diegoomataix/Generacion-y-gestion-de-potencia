ce5eecd2918fa18c5f855ff4fcc561d26ceb3f05
Diego <diego.mataix@hotmail.com>
2021-04-07 21:04:21 +0200

    Se ha intentado

--- Trabajo 4. Efecto Temp y Rad/Datos_3G28C.m
+++ Trabajo 4. Efecto Temp y Rad/Datos_3G28C.m
@@ -21,20 +21,31 @@ for i = 1: size(dat,2)
     V(i,:) = linspace(0,dat(4,i),200);
 end
 
+alpha_Imp = 1e-3*[0.28, 0.36, 0.20, 0.29,0.28 ];
+alpha_Isc = 1e-3*[0.32, 0.33, 0.31, 0.39, 0.32 ];
+alpha_Vmp = 1e-3*[-6.1, -6.8, -6.3, -6.4, -6.1 ];
+alpha_Voc = 1e-3*[-6.0, -6.4, -6.2, -6.3, -6.0 ];
+
+T_0  = [20, 20, 20, 20, 28] + 273.15;             % Temperatura nominal [K]
+
+k = 1.3806503e-23;   % Boltzmann [J/K]
+q = 1.60217646e-19;  % Carga electron [C]
+
+Vt = n.*k.*T_0./q;                        % Voltaje termico
+a_n = 1.3;
 %% DATOS DEL SATÉLITE
 
 h = 450e3;                      % [m]
 RT = 6378e3;                    % [m]
 omega = 0.052;                  % [rad/s]
 mu_T = 5.986e14;                % [m^3/s^2]
-G = 1360;                       % [W/m2]
+G_0 = 1360;                       % [W/m2]
 % PARAMETROS ORBITALES
 % Órbita SS
 a = h + RT;
-n = sqrt(mu_T./a.^3);                   % [rad/s]
 e = 0;
 w = 0;
-T_periodo = 2*pi*sqrt(a.^3/mu_T);       % [s]
-pasoT=1e4;                              % paso temporal
+T_periodo = (2*pi*sqrt(a.^3/mu_T))/100; % [s]
+pasoT=1e2;                              % paso temporal
 T_max = 80+273.15;                      % [K]
 T_min = -20+273.15;                     % [K]
