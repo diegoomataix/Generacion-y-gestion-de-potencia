90bfe4ebd300d175539ac48652313c39ff05595c
Diego <diego.mataix@hotmail.com>
2021-04-15 19:15:39 +0200

    Ap1

--- Trabajo 5. Baterias Modelo Circuito Equivalente/Scripts/LUEGOLEPONEMOSNOMBRE.m
+++ Trabajo 5. Baterias Modelo Circuito Equivalente/Scripts/LUEGOLEPONEMOSNOMBRE.m
@@ -18,19 +18,18 @@ carga1_5A(:,4) = carga1_5A(:,1).*carga1_5A(:,2);
 
 %% APARTADO 1
 V_nom_i = 4.2;
+C_nom_cell = 2750;
 n_serie = round([(max(max(descarga5A(:,3))))/V_nom_i ; (max(max(descarga1_5A(:,3))))/V_nom_i ;  (max(max(descarga2_5A(:,3))))/V_nom_i ])
 
-%Ley de Peukert
-I = 1.5;           %[A]
-t = 36528;         %[s]
-C = [2.750*3600, 2.750*0.97*3600];     %[A·s]
-I = [0.55, 2.75, 5.5, 8,25]
+t = [10615,21365,36528];    % [s]
+I = [5, 2.5, 1.5];
 
-% K = -(log(t)-log(C))/log(I);        % Constante de Peukert
+k = log(t(3)/t(1))/ log(I(1)/I(3));
 
-alpha = log(C(2)/C(1))/log(I(2)/I(1))
+C_p = I.^k.*t;              % [A·s]
+n_par = round(C_p ./ C_nom_cell);
 
-k = (2*alpha+1)/(1+alpha)
+%% APARTADO 2
 
 
 
@@ -42,3 +41,11 @@ k = (2*alpha+1)/(1+alpha)
 % [carga2_5A,~,~]=(xlsread('../ensayos_bateria.xlsx','carga 2.5A'));
 % [descarga1_5A,~,~]=(xlsread('../ensayos_bateria.xlsx','descarga 1.5A'));
 % [carga1_5,~,~]=(xlsread('../ensayos_bateria.xlsx','carga 1.5A'));
+
+% Ley de Peukert
+%I = 1.5;                                         % [A]
+%C_espec = 2.750*3600*[1, 0.97, 0.95, 0.92];      % [A·s]
+%I_espec = [0.55, 2.75, 5.5, 8,25]
+% K = -(log(t)-log(C))/log(I);        % Constante de Peukert
+%alpha_espec = log(C_espec(4)/C_espec(1))/log(I_espec(4)/I_espec(1))
+%k_espec = (2*alpha_espec+1)/(1+alpha_espec)
