ce1c9b1022eaa7fc10f913cb789b11ae939566d5
pazapa <pablo.zapatero.montana@alumnos.upm.es>
2021-04-17 15:21:47 +0200

    dfdadf

--- Trabajo 5. Baterias Modelo Circuito Equivalente/Scripts/AjusteEstaticoBaterias.m
+++ Trabajo 5. Baterias Modelo Circuito Equivalente/Scripts/AjusteEstaticoBaterias.m
@@ -2,14 +2,17 @@ clc; close all; clear all;
 
 %% CARGAR DATOS
 load('descarga5A'); load('carga5A');
-load('descarga2_5A'); load('carga2_5A')
-load('descarga1_5A');load('carga1_5A')
+load('descarga2_5A'); load('carga2_5A');
+load('descarga1_5A');load('carga1_5A');
 
-global limites
-global pesos
-global n_dat
-global caso
-caso = 3;
+global limites; global pesos; global n_dat; global caso;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DESCARGA [1: lineal   % 2: exp. 1era aprox.   % 3: exp. completo ]
+% CARGA    [4: lineal   % 5: exp. completo]
+caso = 5;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+modelo = 2;        % 1: carga        % 2: descarga
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% PROCESAR DATOS
 
@@ -28,10 +31,12 @@ k = log(t(3)/t(1))/ log(I(1)/I(3));
 C_p = I.^k.*t;              % [A·s]
 n_par = round(C_p ./ C_nom_cell);
 
+switch(modelo)
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% APARTADO 2 - DESCARGA
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+case 1
 
 %% LINEAL
 R_d_init = 0.1472;               % [Ohm]
@@ -77,10 +82,48 @@ RMSE2 = fval_exp2
 
 
 end
-%% PLOTS
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% APARTADO 3 - CARGA
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+case 2
+%% LINEAL
+R_c_init = 0.1472;               % [Ohm]
+
+U0 = [0.17547,20,3.6e-03/3600]; %
+t_vect = [carga5A(:, 1); carga2_5A(:, 1); carga1_5A(:, 1)];
+I = [carga5A(:, 2); carga2_5A(:, 2); carga1_5A(:, 2)];
+V_exp = [carga5A(:, 3); carga2_5A(:, 3); carga1_5A(:, 3)];
+phi = [carga5A(:,4);carga2_5A(:,4);carga1_5A(:,4)];
+
+limites = [size(carga5A,1), size(carga2_5A,1), size(carga1_5A,1)];
 lim = [1, limites(1), limites(1)+1, limites(1)+limites(2), limites(1)+limites(2)+1, limites(1)+limites(2)+limites(3)];
 
+n_dat = limites;
+
+switch(caso)
+case 4
+[umin,fval]=fminsearch(@(u) RMSE_V(u,  V_exp, I, phi), U0);
+
+Params_cargal = umin
+RMSE_cargal = fval
+
+case 5
+U1 = [-0.128,20.2091,3.3937e-6, 2.64e-14, 8.44e-2/3600, -3.89e-3/3600]; %
+[umin,fval]=fminsearch(@(u) RMSE_V(u,  V_exp, I, phi), U1);
+
+Params_cargaexp = umin
+RMSE_carga2 = fval
+
+
+end % END SWITCH CASE CASO CARGA
+
+%%%%% END SWITCH CASE CARGA/DESCARGA
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% PLOTS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+lim = [1, limites(1), limites(1)+1, limites(1)+limites(2), limites(1)+limites(2)+1, limites(1)+limites(2)+limites(3)];
 %phi_mat = [phi(lim(1):lim(2)), phi(lim(3):lim(4)), phi(lim(5):lim(6))];
 
 figure()
@@ -89,48 +132,68 @@ grid on
 box on
 
 V_modelo = zeros(limites(1), 1);
-V_modelo_exp1 =  zeros(limites(1), 1);
-V_modelo_exp2 =  zeros(limites(1), 1);
-
 for i=1:lim(2)
-%    V_modelo(i) = lineal(umin,5, phi(i));                        % Parte Lineal
-%    V_modelo_exp1(i) = V_exp1(Params_exp1,5,phi(i));             % Parte Exponencial Mala
-   V_modelo_exp2(i) = V_exp2(Params_exp2,5,phi(i));             % Parte Exponencial Buena
+if caso == 1
+   V_modelo(i) = lineal(umin,5, phi(i));                        % Parte Lineal DESCARGA
+elseif caso == 2
+   V_modelo(i) = V_exp1(Params_exp1,5,phi(i));             % Parte Exponencial Mala DESCARGA
+elseif caso == 3
+   V_modelo(i) = V_exp2(Params_exp2,5,phi(i));             % Parte Exponencial Buena  DESCARGA
+elseif caso == 4
+   V_modelo(i) = V_lineal_carga(Params_cargal,5,phi(i));             % Parte Lineal CARGA
+elseif caso == 5                                                % exp Completo   CARGA
+    V_modelo(i) = V_exp_carga(Params_cargaexp,5,phi(i));     %TBD
 end
-%plot (phi(lim(1):lim(2)), V_modelo, '--k');                    % Parte Lineal
-%plot (phi(lim(1):lim(2)), V_modelo_exp1, '--k');               % Parte Exponencial
-plot (phi(lim(1):lim(2)), V_modelo_exp2, '--k');
+end
+plot (phi(lim(1):lim(2)), V_modelo, '--k');                    % Parte Lineal DESCARGA
+
 
 V_modelo = zeros(limites(2), 1);
-V_modelo_exp1 = zeros(limites(2), 1);
-V_modelo_exp2 = zeros(limites(2), 1);
 for i=lim(3):lim(4)
-%    V_modelo(i-lim(3)+1) = lineal(umin,2.5, phi(i));             % Parte Lineal
-%    V_modelo_exp1(i-lim(3)+1) = V_exp1(Params_exp1,2.5,phi(i));
-   V_modelo_exp2(i-lim(3)+1) = V_exp2(Params_exp2,2.5,phi(i));     % Parte Exponencial
+    if caso == 1
+    V_modelo(i-lim(3)+1) = lineal(umin,2.5, phi(i));             % Parte Lineal DESCARGA
+elseif caso == 2
+    V_modelo(i-lim(3)+1) = V_exp1(Params_exp1,2.5,phi(i));  % Parte Exponencial Mala DESCARGA
+elseif caso == 3
+   V_modelo(i-lim(3)+1) = V_exp2(Params_exp2,2.5,phi(i));   % Parte Exponencial  Buena DESCARGA
+elseif caso == 4
+   V_modelo(i-lim(3)+1) = V_lineal_carga(Params_cargal,2.5,phi(i));
+elseif caso == 5                                                    % exp Completo   CARGA
+   V_modelo(i-lim(3)+1) = V_exp_carga(Params_cargaexp,2.5,phi(i));
 end
-%plot (phi(lim(3):lim(4)), V_modelo, '--k');                    % Parte Lineal
-%plot (phi(lim(3):lim(4)), V_modelo_exp1, '--k');
-plot (phi(lim(3):lim(4)), V_modelo_exp2, '--k');
+end
+plot (phi(lim(3):lim(4)), V_modelo, '--k');
+
+
 
 V_modelo = zeros(limites(3), 1);
-V_modelo_exp1 = zeros(limites(3), 1);
-V_modelo_exp2 = zeros(limites(3), 1);
 for i=lim(5):(lim(6))
-%    V_modelo(i-lim(5)+1) = lineal(umin,1.5, phi(i));             % Parte Lineal
-%    V_modelo_exp1(i-lim(5)+1) = V_exp1(Params_exp1,1.5,phi(i));
-    V_modelo_exp2(i-lim(5)+1) = V_exp2(Params_exp2,1.5,phi(i));    % Parte Exponencial
+    if caso == 1
+    V_modelo(i-lim(5)+1) = lineal(umin,1.5, phi(i));             % Parte Lineal
+elseif caso == 2
+   V_modelo(i-lim(5)+1) = V_exp1(Params_exp1,1.5,phi(i));
+elseif caso == 3
+    V_modelo(i-lim(5)+1) = V_exp2(Params_exp2,1.5,phi(i));    % Parte Exponencial
+elseif caso == 4
+    V_modelo(i-lim(5)+1) = V_lineal_carga(Params_cargal,1.5,phi(i));
+elseif caso == 5
+    V_modelo(i-lim(5)+1) = V_exp_carga(Params_cargaexp,1.5,phi(i)); %TBD
 end
-%plot (phi(lim(5):lim(6)), V_modelo, '--k');
-%plot (phi(lim(5):lim(6)), V_modelo_exp1, '--k');
-plot (phi(lim(5):lim(6)), V_modelo_exp2, '--k');
+end
+plot (phi(lim(5):lim(6)), V_modelo, '--k');
 
 
-% Curvas Experimentales
+if modelo == 1
+    % Curvas Experimentales DESCARGA
 plot (phi(1:limites(1)),V_exp(1:limites(1)),'k');
 plot (phi(limites(1)+1:limites(1)+limites(2)),V_exp( limites(1)+1:limites(1)+limites(2)),'k');
 plot (phi(limites(1)+limites(2)+1:limites(1)+limites(2)+limites(3)),V_exp(limites(1)+limites(2)+1:limites(1)+limites(2)+limites(3)),'k');
-
+elseif modelo == 2
+    % Curvas experimentales de CARGA
+plot (phi(1:limites(1)),V_exp(1:limites(1)),'k');
+plot (phi(limites(1)+1:limites(1)+limites(2)),V_exp( limites(1)+1:limites(1)+limites(2)),'k');
+plot (phi(limites(1)+limites(2)+1:limites(1)+limites(2)+limites(3)),V_exp(limites(1)+limites(2)+1:limites(1)+limites(2)+limites(3)),'k');
+end
 xlabel('{\it V} [V]')
 ylabel('{\it phi} [W·s]');
 %legend({'Aproximación numérica','Datos experimentales'},'Location','northeast','NumColumns',1)
@@ -157,11 +220,15 @@ for i = 1:size(phi,1)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     if caso == 1
 	V_modelo(i) = lineal(u, I(i), phi(i));
-elseif caso == 2
+    elseif caso == 2
     V_modelo(i) = V_exp1(u, I(i), phi(i));
-elseif  caso == 3
+    elseif  caso == 3
     V_modelo(i) = V_exp2(u, I(i), phi(i));
-end
+    elseif caso == 4
+    V_modelo(i) = V_lineal_carga(u, I(i), phi(i));
+    elseif caso == 5
+    V_modelo(i) = V_exp_carga(u, I(i), phi(i));
+    end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	if i == limites(1)
 		V_mod_err = V_modelo(1:limites(1));
@@ -185,7 +252,6 @@ end
 error = error_vect(1) + error_vect(2)+ error_vect(3);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 function V_lineal = lineal(u, I, phi)
 global Vt
 %R_d = u(1); E_d0 = u(2); E_d1 = u(3);
@@ -207,39 +273,19 @@ E_d = u(2) + u(3)*phi + (u(4) + u(5)*I + u(6)*I^2)*exp((u(7) + u(8)*I)*phi);
 V_exp2 = E_d - u(1)*I;
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% function error = RMSE_exp1(u, V_exp, I, phi)
-% global n_dat; global pesos; global limites;
-% %V_modelo = zeros (1,sum(limites));
-% for i = 1:size(phi,1)
-% 	if i <= limites(1)
-% 		j = 1;
-% 	elseif i <= limites(2) + limites(1)
-% 		j = 2;
-% 	else
-% 		j = 3;
-% 	end
-% 	V_modelo(i) = V_exp1(u, I(i), phi(i));
-% 	if i == limites(1)
-% 		V_mod_err = V_modelo(1:limites(1));
-% 		V_exp_err = V_exp(1:limites(1));
-% 		error_vect(j) = ((sum (( V_mod_err' - V_exp_err).^2 ) / n_dat(j))^0.5);
-%
-% 	elseif i == limites(1) + limites(2)
-% 		V_mod_err = V_modelo( limites(1)+1:limites(1)+limites(2));
-% 		V_exp_err = V_exp( limites(1)+1:limites(1)+limites(2));
-% 		error_vect(j) = ((sum (( V_mod_err' - V_exp_err).^2 ) / n_dat(j))^0.5);
-%
-% 	elseif  i == sum(limites)
-% 		V_mod_err = V_modelo(limites(1)+limites(2)+1:limites(1)+limites(2)+limites(3));
-% 		V_exp_err = V_exp(limites(1)+limites(2)+1:limites(1)+limites(2)+limites(3));
-% 		error_vect(j) = (sum((V_mod_err' - V_exp_err).^2)/n_dat(j))^0.5;
-%      else
-% 	end
-%
-% end
-%
-% error = error_vect(1) + error_vect(2)+ error_vect(3);
-% end
+function V_lineal_carga = V_lineal_carga(u, I, phi)
+global Vt
+%R_c = u(1); E_c0 = u(2); E_c1 = u(3);
+E_c = u(2) + u(3)*phi;
+V_lineal_carga = E_c - u(1)*I;
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function V_exp_carga = V_exp_carga(u, I, phi)
+global Vt
+%R_c = u(1); E_c0 = u(2); E_c1 = u(3); E_c2_0=u(4); E_c3_1=u(5); E_3_2=u(6);
+E_c = u(2) + u(3)*phi+u(4)*exp((u(5)+u(6)*I)*phi);
+V_exp_carga = E_c - u(1)*I;
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% BACKUP
 % Ley de Peukert
@@ -288,3 +334,37 @@ end
 
 % Params_exp1 = [U_lin,umin_exp1]
 % RMSE2 = fval_exp1
+
+% function error = RMSE_exp1(u, V_exp, I, phi)
+% global n_dat; global pesos; global limites;
+% %V_modelo = zeros (1,sum(limites));
+% for i = 1:size(phi,1)
+% 	if i <= limites(1)
+% 		j = 1;
+% 	elseif i <= limites(2) + limites(1)
+% 		j = 2;
+% 	else
+% 		j = 3;
+% 	end
+% 	V_modelo(i) = V_exp1(u, I(i), phi(i));
+% 	if i == limites(1)
+% 		V_mod_err = V_modelo(1:limites(1));
+% 		V_exp_err = V_exp(1:limites(1));
+% 		error_vect(j) = ((sum (( V_mod_err' - V_exp_err).^2 ) / n_dat(j))^0.5);
+%
+% 	elseif i == limites(1) + limites(2)
+% 		V_mod_err = V_modelo( limites(1)+1:limites(1)+limites(2));
+% 		V_exp_err = V_exp( limites(1)+1:limites(1)+limites(2));
+% 		error_vect(j) = ((sum (( V_mod_err' - V_exp_err).^2 ) / n_dat(j))^0.5);
+%
+% 	elseif  i == sum(limites)
+% 		V_mod_err = V_modelo(limites(1)+limites(2)+1:limites(1)+limites(2)+limites(3));
+% 		V_exp_err = V_exp(limites(1)+limites(2)+1:limites(1)+limites(2)+limites(3));
+% 		error_vect(j) = (sum((V_mod_err' - V_exp_err).^2)/n_dat(j))^0.5;
+%      else
+% 	end
+%
+% end
+%
+% error = error_vect(1) + error_vect(2)+ error_vect(3);
+% end
