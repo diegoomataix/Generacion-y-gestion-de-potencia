858aaeb1de2b6f5d22eade471b302eb2b4672e3a
JavierVeg <76919514+JavierVegam@users.noreply.github.com>
2021-05-09 14:12:15 +0200

    Trabajo 7 y Modelo 2C2R

--- Trabajo 7. Modelo de conversores DCDC/scripts/Ajuste_DCDC.m
+++ Trabajo 7. Modelo de conversores DCDC/scripts/Ajuste_DCDC.m
@@ -49,6 +49,7 @@ limites = [size(I1_33,1), size(I1_5,1), size(I1_15,1)];
 n_dat = limites
 
 
+% Esto a tomar por culo de aqui
 eta_exp = [eta_33; eta_5 ; eta_15];
 eta_max = [max(eta_33), max(eta_5), max(eta_15)];
 V_exp = [V2_33  ; V2_5  ; V2_15];
@@ -56,85 +57,171 @@ P_exp =  [P2_33  ; P2_5  ; P2_15];
 
 %% Escoger el metodo de ajuste
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-caso = 3;
+caso = 2;          % 1. Lineal 2. Polinomico 2º orden 3. Polinomico 3er orden
+%dispositivo = 1;   % 1. 3,3 V  2. 5 V 3.15V
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for caso = 1:3
+
+for dispositivo = 1:3
 switch (caso)
 case 1
-U0 = -0.5;
-[umin,fval]=fminsearch(@(u) RMSE_eta(u, eta_exp, P_exp, V_exp), U0);
 
-Param_1 = umin
-RMSE_1 = fval
+	if dispositivo ==1
+U0 = -0.5;
+[umin,fval]=fminsearch(@(u) RMSE_eta(u, eta_33, P2_33, V2_33), U0);
+
+Param_1_33 = umin
+RMSE_1_33 = fval
+
+elseif dispositivo ==2
+U0 = -0.5;
+[umin,fval]=fminsearch(@(u) RMSE_eta(u, eta_5, P2_5, V2_5), U0);
+
+Param_1_5 = umin
+RMSE_1_5 = fval
+
+elseif dispositivo ==3
+U0 = -0.5;
+[umin,fval]=fminsearch(@(u) RMSE_eta(u, eta_15, P2_15, V2_15), U0);
+
+Param_1_15 = umin
+RMSE_1_15 = fval
+end
 
 case 2
 
+if dispositivo == 1
 U_2 = [-0.5, 0.1];
-[umin,fval]=fminsearch(@(u) RMSE_eta(u, eta_exp, P_exp, V_exp), U_2);
+[umin,fval]=fminsearch(@(u) RMSE_eta(u, eta_33, P2_33, V2_33), U_2);
 
-Param_2 = umin
-RMSE_2 = fval
+Param_2_33 = umin
+RMSE_2_33 = fval
+
+elseif dispositivo == 2
+U_2 = [-0.5, 0.1];
+[umin,fval]=fminsearch(@(u) RMSE_eta(u, eta_5, P2_5, V2_5), U_2);
+
+Param_2_5 = umin
+RMSE_2_5 = fval
+
+elseif dispositivo == 3
+U_2 = [-0.5, 0.1];
+[umin,fval]=fminsearch(@(u) RMSE_eta(u, eta_15, P2_15, V2_15), U_2);
+
+Param_2_15 = umin
+RMSE_2_15 = fval
+end
+
 
 case 3
 
+if dispositivo == 1
 U_3 = [-0.5, 0.1, 0.1];
-[umin,fval]=fminsearch(@(u) RMSE_eta(u, eta_exp, P_exp, V_exp), U_3);
+[umin,fval]=fminsearch(@(u) RMSE_eta(u, eta_33, P2_33, V2_33), U_3);
 
-Param_3 = umin
-RMSE_3= fval
+Param_3_33 = umin
+RMSE_3_33 = fval
+
+elseif dispositivo == 2
+	U_3 = [-0.5, 0.1, 0.1];
+	[umin,fval]=fminsearch(@(u) RMSE_eta(u, eta_5, P2_5, V2_5), U_3);
+
+	Param_3_5 = umin
+	RMSE_3_5 = fval
+
+elseif dispositivo == 3
+
+	U_3 = [-0.5, 0.1, 0.1];
+	[umin,fval]=fminsearch(@(u) RMSE_eta(u, eta_15, P2_15, V2_15), U_3);
+
+	Param_3_15 = umin
+	RMSE_3_15 = fval
+end
+
+end
 end
 
 %% PLOTS
 
-eta_modelo = zeros (1,sum(limites));
-for i = 1:size(eta_exp,1)
-	if i <= limites(1)
-		j = 1;
-	elseif i <= limites(2) + limites(1)
-		j = 2;
-	else
-		j = 3;
-	end
 if caso == 1
-eta_modelo(i) = DCDC(Param_1, V_exp(i), P_exp(i), eta_max(j));
+
+eta_modelo_33_c1 = DCDC(Param_1_33, V2_33, P2_5, max(eta_33));
+eta_modelo_5_c1 = DCDC(Param_1_5, V2_5, P2_5, max(eta_5));
+eta_modelo_15_c1 = DCDC(Param_1_15, V2_15, P2_15, max(eta_15));
+
 elseif caso == 2
-eta_modelo(i) = DCDC2(Param_2, V_exp(i), P_exp(i), eta_max(j));
-elseif  caso == 3
-eta_modelo(i) = DCDC3(Param_3, V_exp(i), P_exp(i), eta_max(j));
+
+eta_modelo_33_c2 = DCDC2(Param_2_33, V2_33, P2_33, max(eta_33));
+eta_modelo_5_c2  = DCDC2(Param_2_5, V2_5, P2_5, max(eta_5));
+eta_modelo_15_c2 = DCDC2(Param_2_15, V2_15, P2_15, max(eta_15));
+
+elseif caso == 3
+
+eta_modelo_33_c3 = DCDC3(Param_3_33, V2_33, P2_33, max(eta_33));
+eta_modelo_5_c3  = DCDC3(Param_3_5, V2_5, P2_5, max(eta_5));
+eta_modelo_15_c3 = DCDC3(Param_3_15, V2_15, P2_15, max(eta_15));
+
 end
 end
+%end CASO
 
-figure()
+
+figure(1)
 hold on
-plot(P2_33,eta_33,  '--k')
-plot(P2_33,eta_modelo(1:limites(1)))
+grid on
+plot(P2_33,eta_33, '-k', 'LineWidth' ,1.5)
+plot(P2_33,eta_modelo_33_c1, '--*k')
+plot(P2_33,eta_modelo_33_c2, '--*k')
+plot(P2_33,eta_modelo_33_c3, '--dk')
+set(gca,'FontSize',18)
+xlabel('\it {P_{out}} [W]')
+ylabel('\eta')
+legend('Datos experimentales', 'Ajuste 1' ,'Ajuste 2', 'Ajuste 3')
 hold off
 
-figure()
+figure(2)
 hold on
-plot(P2_5,eta_5, '--k')
-plot(P2_5,eta_modelo(limites(1)+1:limites(1)+limites(2)))
+grid on
+plot(P2_5,eta_5, '-k','LineWidth', 1.5)
+plot(P2_5,eta_modelo_5_c1,'--*k')
+plot(P2_5,eta_modelo_5_c2, '--+k')
+plot(P2_5,eta_modelo_5_c3, '--dk')
+set(gca,'FontSize',18)
+xlabel('\it {P_{out}} [W]')
+ylabel('\eta')
+legend('Datos experimentales', 'Ajuste 1', 'Ajuste 2', 'Ajuste 3')
 hold off
 
-figure()
+
+figure(3)
 hold on
-plot(P2_5,eta_5,  '--k')
-plot(P2_15,eta_modelo(limites(1)+limites(2)+1:limites(1)+limites(2)+limites(3)))
+grid on
+plot(P2_15,eta_15, '-k' , 'LineWidth', 1.5)
+plot(P2_15,eta_modelo_15_c1, '--*k')
+plot(P2_15,eta_modelo_15_c2,'--+k')
+plot(P2_15,eta_modelo_15_c3, '--dk')
+set(gca,'FontSize',18)
+xlabel('\it {P_{out}} [W]')
+ylabel('\eta')
+legend('Datos experimentales', 'Ajuste 1', 'Ajuste 2', 'Ajuste 3' )
 hold off
+
+%end
 
 %Funciones
 
 function eta_ajust_1 = DCDC(u, V2, P2, eta_max)
-f  = (u(1)/V2)*P2;
+f  = u(1).*(P2./V2);
 eta_ajust_1 = eta_max*(1-exp(f));
 end
 
 function eta_ajust_2 = DCDC2(u, V2, P2, eta_max)
-f = (u(1)/V)*P2 + (u(2)/V2)*P2^2;
+f = u(1).*(P2./V2) + u(2).*(P2./V2).^2;
 eta_ajust_2 = eta_max*(1-exp(f));
 end
 
 function eta_ajust_3 = DCDC3(u, V2, P2, eta_max)
-f = (u(1)/V2)*P2 + (u(2)/V2)*P2^2 + (u(3)/V2)*P2^3;
+f = (u(1)./V2).*P2 + u(2).*(P2./V2).^2 + u(3).*(P2./V2).^3;
 eta_ajust_3 = eta_max*(1-exp(f));
 end
 
@@ -142,44 +229,24 @@ function error = RMSE_eta(u, eta_exp, P_exp, V_exp)
 global n_dat; global limites; global caso
 global eta_max
 
-eta_modelo = zeros (1,sum(limites));
+eta_modelo = zeros (1,size(eta_exp,1));
+
 for i = 1:size(eta_exp,1)
-	if i <= limites(1)
-		j = 1;
-	elseif i <= limites(2) + limites(1)
-		j = 2;
-	else
-		j = 3;
-	end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     if caso == 1
-	eta_modelo(i) = DCDC(u, V_exp(i), P_exp(i), eta_max(j))  ;
+	eta_modelo(i) = DCDC(u, V_exp(i), P_exp(i), max(eta_exp))  ;
     elseif caso == 2
-    eta_modelo(i) = DCDC2(u, V_exp(i), P_exp(i), eta_max(j));
+    eta_modelo(i) = DCDC2(u, V_exp(i), P_exp(i), max(eta_exp));
     elseif  caso == 3
-    eta_modelo(i) = DCDC3(u, V_exp(i), P_exp(i), eta_max(j));
+    eta_modelo(i) = DCDC3(u, V_exp(i), P_exp(i), max(eta_exp));
     end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-	if i == limites(1)
-		eta_mod_err = eta_modelo(1:limites(1));
-		eta_exp_err = eta_exp(1:limites(1));
-		error_vect(j) = ((sum (( eta_mod_err' - eta_exp_err).^2 ) / n_dat(j))^0.5);
-
-	elseif i == limites(1) + limites(2)
-		eta_mod_err = eta_modelo( limites(1)+1:limites(1)+limites(2));
-		eta_exp_err = eta_exp( limites(1)+1:limites(1)+limites(2));
-		error_vect(j) = ((sum (( eta_mod_err' - eta_exp_err).^2 ) / n_dat(j))^0.5);
-
-	elseif  i == sum(limites)
-		eta_mod_err = eta_modelo(limites(1)+limites(2)+1:limites(1)+limites(2)+limites(3));
-		eta_exp_err = eta_exp(limites(1)+limites(2)+1:limites(1)+limites(2)+limites(3));
-		error_vect(j) = ((sum (( eta_mod_err' - eta_exp_err).^2 ) / n_dat(j))^0.5);
-     else
-	end
-
 end
+		eta_mod_err = eta_modelo;
+		eta_exp_err = eta_exp;
+		error_vect = ((sum (( eta_mod_err' - eta_exp_err).^2 ) / size(eta_exp,1))^0.5);
 
-error = error_vect(1) + error_vect(2)+ error_vect(3);
+% Error resultante
+error = error_vect;
 end
