632d802c328db1ae6dc47fdb67bc363437da2362
pazapa <pablo.zapatero.montana@alumnos.upm.es>
2021-05-13 18:16:04 +0200

    zcxczx

--- Trabajo 6. Baterias Ajuste Dinamico/scripts/MATapoyo.m
+++ Trabajo 6. Baterias Ajuste Dinamico/scripts/MATapoyo.m
@@ -198,7 +198,6 @@ legend('Datos experimentales' , 'Modelo 1RC analítico')
 set(gca,'FontSize',18)
 hold off
 
-
 figure()
 box on
 grid on
@@ -235,15 +234,6 @@ E_d2_1 = -1.603e-9;
 E_d2_2 = 2.1092e-10;
 E_d3_0 = 1.834e-5;
 E_d3_1 = 2.019e-15;
-%Con phi2 correcto
-% R_d = [0.118667455470931];
-% E_d0 = [24.3];
-% E_d1 = [-3.51909965949880e-06];
-% E_d2_0 = [5.03123121278738e-10];
-% E_d2_1 = [-6.84502902638074e-10];
-% E_d2_2 = [9.60096310808297e-11];
-% E_d3_0 = [1.87750230665809e-05];
-% E_d3_1 = [-1.68795045318578e-13];
 
 %Carga
 R_c = 0.0875;
@@ -261,9 +251,6 @@ for i=1:size(I_D,1)
     if I_D(i)>0
     E(i)= E_d0 + E_d1*phi_d(i) + (E_d2_0 + E_d2_1*abs(I_D(i)) + E_d2_2*I_D(i)^2)*exp((E_d3_0 + E_d3_1*abs(I_D(i)))*phi_d(i));
     V(i) = E(i) - R_d*(I_D(i));
-    %I_D(i)
-    %E(i) = E_d0 + E_d1*phi(i);
-    %V(i) = E(i) - R_d*abs(I_D(i));
 
     else
     E(i)= E_c0 - E_c1*phi_c(i)-E_c2_0*exp((E_c3_1+E_c3_2*abs(I_D(i)))*phi_c(i));
@@ -312,103 +299,4 @@ xlabel('{\it t} [s]')
 ylabel('{\it V} [V]')
 hold off
 
-%% BACKUP
-% figure()
-% box on
-% grid on
-% plot(t_1,V_1)
-% figure()
-% box on
-% grid on
-% plot(t_2,V_2)
-% figure()
-% box on
-% grid on
-% plot(t_3,V_3)
-% figure()
-% box on
-% grid on
-% plot(t_4,V_4)
-% figure()
-% box on
-% grid on
-% plot(t_5,V_5)
-% figure()
-% box on
-% grid on
-% plot(t_6,V_6)
-%
-%  function plot_result = plot_fun(axisx,axisy,labelx,labely,yourtitle)
-%  figure()
-% 
-%  axis([min(axisx) max(axisx)  min(axisy)*0.95 max(axisy)*1.05])
-%  xlabel(labelx)
-%  ylabel(labely)
-%  title(yourtitle)
-%  box on
-%  set(gca,'FontSize',18)
-%  grid on
-%  grid minor
-%  hold on
-%  plot_result = plot(axisx,axisy,'k','LineWidth',2);
-%  end
 
-% V1_rect = -0.00048441.*t_1 + 23.97875522;
-% V2_rect = 0.0000701410.*t_2 + 23.6394221899;
-% V3_rect = -0.00003619.*t_3 + 23.92519957;
-% V4_rect = -0.000461*t_4 + 25.384720;
-% V5_rect = 0.0000879957.*t_5 + 23.1738203106;
-% V6_rect = -0.00021592.*t_6 + 24.76426337;
-%
-% V_1 = V_1 - V1_rect;
-% V_2 = V_2 - V2_rect;
-% V_3 = V_3 - V3_rect;
-% V_4 = V_4 - V4_rect;
-% V_5 = V_5 - V5_rect;
-% V_6 = V_6 - V6_rect;
-%
-% V_1 = V_1 + abs(min(V_1));
-% V_2 = abs(V_2 - max(V_2));
-% V_3 = V_3 + abs(min(V_3));
-% V_4 = V_4 + abs(min(V_4));
-% V_5 = abs(V_5 - max(V_5));
-% V_6 = V_6 + abs(min(V_6));
-%
-% V_1 = smooth(V_1,80);
-% V_2 = smooth(V_2,80);
-% V_3 = smooth(V_3,80);
-% V_4 = smooth(V_4,80);
-% V_5 = smooth(V_5,80);
-% V_6 = smooth(V_6,80);
-%
-% R12 = max(V_2)/abs(I_1(300)-I_2(300));
-% R13 = max(V_3)/abs(I_2(300)-I_3(300));
-% R14 = max(V_4)/abs(I_3(300)-I_4(300));
-% R15 = max(V_5)/abs(I_4(300)-I_5(300));
-% R16 = max(V_6)/abs(I_5(300)-I_6(300));
-%
-% R_inc = (R12 + R13 + R14 + R15 + R16) /5 ;
-%
-% Dv_Di_2 = V_2./abs(I_1(300)-I_2(300));
-% Dv_Di_3 = V_3./abs(I_2(300)-I_3(300));
-% Dv_Di_4 = V_4./abs(I_3(300)-I_4(300));
-% Dv_Di_5 = V_5./abs(I_4(300)-I_5(300));
-% Dv_Di_6 = V_6./abs(I_5(300)-I_6(300));
-%
-% [a,Pos2] = min(abs(Dv_Di_2-0.368*max(Dv_Di_2)));
-% [b,Pos3] = min(abs(Dv_Di_3-0.368*max(Dv_Di_3)));
-% [c,Pos4] = min(abs(Dv_Di_4-0.368*max(Dv_Di_4)));
-% [d,Pos5] = min(abs(Dv_Di_5-0.368*max(Dv_Di_5)));
-% [e,Pos6] = min(abs(Dv_Di_6-0.368*max(Dv_Di_6)));
-%
-% C12 = t_2(Pos2)/R12;
-% C13 = t_3(Pos3)/R13;
-% C14 = t_4(Pos4)/R14;
-% C15 = t_5(Pos5)/R15;
-% C16 = t_6(Pos6)/R16;
-
-%plot_fun(t_2,Dv_Di_2,'','','')
-%plot_fun(t_3,Dv_Di_3,'','','')
-%plot_fun(t_4,Dv_Di_4,'','','')
-%plot_fun(t_5,Dv_Di_5,'','','')
-%plot_fun(t_6, Dv_Di_6,'','','')
